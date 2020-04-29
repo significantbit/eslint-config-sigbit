e33b616c05b7386b9410c9e33892e2a31a1c01dc
Jacob Andersson <jacob.m.andersson@gmail.com>
2020-04-29 21:59:19 +0200

    Init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Sigbit ESLint config
+> 👮‍♂️ Enforce code style
+
+## Usage
+Install from GitHub:
+```sh
+$ yarn add significantbit/eslint-config-sigbit
+```
+
+Use default config:
+```js
+// .eslintrc.js
+module.exports = {
+  extends: [
+    "sigbit"
+  ]
+}
+```
+
+Use specific config:
+```js
+// .eslintrc.js
+module.exports = {
+  extends: [
+    "sigbit/nuxt"
+  ]
+}
+```
---
+++ nuxt.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript"
+  ],
+  // add your custom rules here
+  rules: {
+    // Prevent console from being used in production
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+    // Enforce use of comma
+    // https://eslint.org/docs/rules/comma-dangle#always-multiline
+    "comma-dangle": ["error", "always-multiline"],
+
+    // https://eslint.org/docs/rules/object-curly-newline#consistent
+    "object-curly-newline": ["error", { "consistent": true }],
+
+    // https://eslint.org/docs/rules/curly#consistent
+    "curly": ["error", "multi", "consistent"],
+
+    // Enforce use of semicolons
+    // https://eslint.org/docs/rules/semi#omitlastinonelineblock
+    "semi": ["error", "always", { "omitLastInOneLineBlock": true }],
+
+    // https://eslint.org/docs/rules/max-len
+    "max-len": ["error", { code: 150 }],
+
+    // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/no-v-html.md
+    "vue/no-v-html": "off",
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+    "@typescript-eslint/explicit-function-return-type": "off",
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/ban-ts-ignore.md
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    "@typescript-eslint/no-unused-vars": "warn"
+  }
+}
